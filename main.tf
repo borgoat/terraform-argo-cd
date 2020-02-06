ebc47fdd2ce66b1393420cbb05061eddb0699d7b
Giorgio Azzinnaro <giorgio.azzinnaro@gmail.com>
2020-02-06 22:01:33 +0100

    move to terraform 3

--- main.tf
+++ main.tf
@@ -1,31 +1,5 @@
-resource "kubernetes_service_account" "tiller" {
-  metadata {
-    name      = "tiller"
-    namespace = "kube-system"
-  }
-}
-
-resource "kubernetes_cluster_role_binding" "tiller" {
-  metadata {
-    name = "tiller"
-  }
-
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
-
-  subject {
-    kind      = "ServiceAccount"
-    name      = kubernetes_service_account.tiller.metadata.0.name
-    namespace = kubernetes_service_account.tiller.metadata.0.namespace
-  }
-}
-
 provider "helm" {
-  namespace       = kubernetes_cluster_role_binding.tiller.subject.0.namespace
-  service_account = kubernetes_cluster_role_binding.tiller.subject.0.name
+  version = "~> 1.0"
 }
 
 data "helm_repository" "argoproj" {
